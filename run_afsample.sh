65d4b52de26cd23d6ecead43470f49a4dc35acac
Björn Wallner <bjorn.wallner@liu.se>
2022-12-19 14:22:03 +0100

    update of run scripts

--- run_afsample.sh
+++ run_afsample.sh
@@ -2,9 +2,9 @@
 
 fasta=$1
 outfolder=AF_models_dropout/ 
-#UPDATE the DOWNLOAD_DIR to the location of the databases:
+#UPDATE the DOWNLOAD_DIR to the location of the AF databases:
 DOWNLOAD_DIR=/proj/wallner-b/share/alphafold_data/
-
+AF_path=/proj/wallner-b/apps/alphafoldv.2.2.0/
 std_flags="
 --db_preset=full_dbs
 --uniclust30_database_path=$DOWNLOAD_DIR/uniclust30/UniRef30_2021_06/UniRef30_2021_06
@@ -13,6 +13,8 @@ std_flags="
 --uniprot_database_path=$DOWNLOAD_DIR/alphafold_data/uniprot/uniprot.fasta
 "
 
+#echo $std_flags
+#exit
 
 #Create the MSAs and template search for the $fasta and stop
 #python run_alphafold.py $std_flags --model_preset multimer --fasta_paths $fasta --output_dir AF_models_dropout/ --seq_only
@@ -21,17 +23,19 @@ std_flags="
 
 #Alternatively, the MSA will be created by the first run before the inference starts
 #generate 10x200 models using v1 & v2 using dropout and templates
-python run_alphafold.py $std_flags --model_preset multimer_all --fasta_paths $fasta --output_dir $outfolder/ --nstruct 200 --dropout=True
+python $AF_path/run_alphafold.py $std_flags --model_preset multimer_all --fasta_paths $fasta --output_dir $outfolder/ --nstruct 200 --dropout=True
 
 #generate 10x200 models using v1 & v2 using dropout and notemplates, the --suffix ensures the model names are different since outfolder is the same
-python run_alphafold.py $std_flags --model_preset multimer_all --fasta_paths $fasta --output_dir $outfolder/ AF_models_dropout/ --nstruct 200 --dropout=True --no_templates --suffix no_templates
+python$AF_path/ run_alphafold.py $std_flags --model_preset multimer_all --fasta_paths $fasta --output_dir $outfolder/ AF_models_dropout/ --nstruct 200 --dropout=True --no_templates --suffix no_templates
 
 #generate 5x200 models using v1 using dropout, notemplates, recycles 21, the --suffix ensures the model names are different since outfolder is the same
-python run_alphafold.py $std_flags --model_preset multimer_v1 --fasta_paths $fasta --output_dir $outfolder/ --nstruct 200 --dropout=True --no_templates --max_recycles 21 --suffix no_templates_r21
+python $AF_path/run_alphafold.py $std_flags --model_preset multimer_v1 --fasta_paths $fasta --output_dir $outfolder/ --nstruct 200 --dropout=True --no_templates --max_recycles 21 --suffix no_templates_r21
 
 #generate 5x200 models using v2 using dropout, notemplates, recycles 9, the --suffix ensures the model names are different since outfolder is the same
-python run_alphafold.py $std_flags --model_preset multimer_v2 --fasta_paths $fasta --output_dir $outfolder/ --nstruct 200 --dropout=True --no_templates --max_recycles 9 --suffix no_templates_r9
+python $AF_path/run_alphafold.py $std_flags --model_preset multimer_v2 --fasta_paths $fasta --output_dir $outfolder/ --nstruct 200 --dropout=True --no_templates --max_recycles 9 --suffix no_templates_r9
+
+#get the score for all models and return a sorted scorefile.
+python $AF_path/scores_from_json.py $fasta $outfolder/ > $outfolder/scores.sc
 
 
-python scores_from_json.py $outfolder/
 
--- scores_from_json.py
+++ scores_from_json.py
@@ -5,17 +5,22 @@ import json
 import pathlib
 import sys
 import glob
+import tqdm
 
 fasta=sys.argv[1]
 output_dir_base=sys.argv[2]
 
-fasta_names = pathlib.Path(fasta).stem
+fasta_name = pathlib.Path(fasta).stem
 output_jsons = os.path.join(output_dir_base, fasta_name,'result*.pkl.json')
-
-for jsons in glob.glob(output_jsons):
-    with open(jsons,'rb') as f:
+scores={}
+for json_file in tqdm.tqdm(glob.glob(output_jsons)):
+    with open(json_file,'rb') as f:
         d=json.load(f)
-        #        print(jsons,d)
-        model=jsons.replace('result','unrelaxed').replace('.pkl.json','.pdb')
-        print(model,d['ranking_confidence'])
-    
+        model=json_file.replace('result','unrelaxed').replace('.pkl.json','.pdb')
+        try:
+            scores[model]=float(d['ranking_confidence'])
+        except:
+            print(f'ranking_confidence not found in {json_file}')
+
+for model,score in sorted(scores.items(),key=lambda x:x[1],reverse=True):
+    print(model,score)
